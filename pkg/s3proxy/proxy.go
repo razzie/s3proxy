132ad51402f538f86a3bdb02dd85830330d3f315
Gábor Görzsöny <gabor@gorzsony.com>
2023-02-14 22:16:40 +0100

    Assume https scheme for endpoint if missing

--- pkg/s3proxy/proxy.go
+++ pkg/s3proxy/proxy.go
@@ -9,14 +9,16 @@ import (
 )
 
 type Proxy struct {
-	endpoint *url.URL
-	lt       *LookupTable
-	proxy    *httputil.ReverseProxy
+	lt    *LookupTable
+	proxy *httputil.ReverseProxy
 }
 
 func NewProxy(endpoint, encryptionKey string) (*Proxy, error) {
 	if len(endpoint) == 0 {
 		return nil, errors.New("missing endpoint")
+	}
+	if !strings.HasPrefix(endpoint, "https://") && !strings.HasPrefix(endpoint, "http://") {
+		endpoint = "https://" + endpoint
 	}
 	u, err := url.Parse(endpoint)
 	if err != nil {
@@ -28,6 +30,7 @@ func NewProxy(endpoint, encryptionKey string) (*Proxy, error) {
 		origDirector := p.Director
 		p.Director = func(r *http.Request) {
 			origDirector(r)
+			r.Host = u.Host
 			r.Header.Del("Content-MD5")
 		}
 		p.ModifyResponse = func(r *http.Response) error {
@@ -36,14 +39,12 @@ func NewProxy(endpoint, encryptionKey string) (*Proxy, error) {
 		}
 	}
 	return &Proxy{
-		endpoint: u,
-		lt:       lt,
-		proxy:    p,
+		lt:    lt,
+		proxy: p,
 	}, nil
 }
 
 func (p Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r.Host = p.endpoint.Host
 	if r.Method == "PUT" {
 		r.Body = &ReadCloser{R: r.Body, F: p.lt.Encrypt}
 	} else if r.Method == "GET" && !strings.HasSuffix(r.URL.Path, "/") {
